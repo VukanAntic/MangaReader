1de9e100e67d2fd7278c90406ef0e813eb01d2cc
Vukan Antić <61616630+VukanAntic@users.noreply.github.com>
2023-05-07 15:43:47 +0200

    Add: Dependency injection

--- MangaReader/MangaCatalog.API/Program.cs
+++ MangaReader/MangaCatalog.API/Program.cs
@@ -1,6 +1,13 @@
+using MangaCatalog.API.Data;
+using MangaCatalog.API.Repositories;
+using MangaCatalog.API.Repositories.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddScoped<IMangaCatalogContext, MangaCatalogContext>();
+builder.Services.AddScoped<IMangaRepository, MangaRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
