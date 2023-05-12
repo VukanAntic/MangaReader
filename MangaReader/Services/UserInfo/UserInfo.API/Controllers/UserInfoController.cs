cd09be3360c6261459031aa621c01bf9aabb20ce
KatarinaDimitrijevic <katarina.dimitrijeviiic@gmail.com>
2023-05-12 23:22:38 +0200

    Added basic endpoints - created controller

---
+++ MangaReader/Services/UserInfo/UserInfo.API/Controllers/UserInfoController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserInfo.API.Entities;
+using UserInfo.API.Repository;
+
+namespace UserInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class UserInfoController : ControllerBase
+    {
+        private readonly IUserInformationRepository _repository;
+
+        public UserInfoController(IUserInformationRepository repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(UserInformation), StatusCodes.Status200OK)]
+        public async Task<ActionResult<UserInformation>> GetUserInfo(string id) 
+        {
+            var userInfo = await _repository.GetUserInfo(id);
+            return Ok(userInfo ?? new UserInformation(id));
+        }
+
+
+        [HttpPut]
+        [ProducesResponseType(typeof(UserInformation), StatusCodes.Status200OK)]
+        public async Task<ActionResult<UserInformation>> UpdateUserInfo([FromBody] UserInformation userInformation)
+        {
+            return Ok(await _repository.UpdateUserInfo(userInformation));
+        }
+
+
+        [HttpDelete]
+        [ProducesResponseType(typeof(void), StatusCodes.Status200OK)]
+        public async Task<IActionResult> DeleteUserInfo(string id)
+        {
+            await _repository.DeleteUserInfo(id);
+            return Ok();
+        }
+
+
+
+    }
+}
