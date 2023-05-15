8f90dcc906bce1adb55aa331313d2522421cb5c0
Mirjana Jocovic <mi18135@alas.matf.bg.ac.rs>
2023-05-15 16:23:45 +0200

    Revert "Merge branch 'userInfo' into develop"
    This reverts commit a86c4032848d2c15b64494301b3bcebf04fec6c0, reversing
    changes made to af7cac9a7895b0173f4509e37bb08414e14e7118.

--- MangaReader/Services/UserInfo/UserInfo.API/DTOs/CreateUserInfoDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace UserInfo.API.DTOs
-{
-	public class CreateUserInfoDTO
-	{
-		public string userId { get; set; } = string.Empty;
-	}
-}
-
--- MangaReader/Services/UserInfo/UserInfo.API/Entities/ReadingJournal.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UserInfo.API.Entities
-{
-    public class ReadingJournal
-    {
-        public string LastReadMangaID { get; set; } = string.Empty;
-        public List<string> AllReadMangaIDs { get; set; } = new List<string>();
-        public List<string> WishList { get; set; } = new List<string>();
-    }
-}
