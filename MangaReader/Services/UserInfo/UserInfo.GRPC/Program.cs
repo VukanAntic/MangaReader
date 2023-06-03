6eb2b76d2783a7feb303f7b09e692c90afd59336
Vukan Antić <61616630+VukanAntic@users.noreply.github.com>
2023-06-03 17:10:23 +0200

    Add: Added Grpc server-side for UserInfo

--- MangaReader/Services/UserInfo/UserInfo.GRPC/Program.cs
+++ MangaReader/Services/UserInfo/UserInfo.GRPC/Program.cs
@@ -1,4 +1,5 @@
 using UserInfo.GRPC.Services;
+using UserInfo.API.Extentions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,11 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+// FIX NEEDED!
+builder.Services.Mapper(builder.Configuration);
+//builder.Services.AddUserInfoCommonServices(builder.Configuration);
+// TODO: check if we need mapper, because it is never used
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<UserInfoService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
---
+++ MangaReader/Services/UserInfo/UserInfo.GRPC/Services/UserInfoService.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserInfo.API.Repository;
+using UserInfo.GRPC.Protos;
+
+namespace UserInfo.GRPC.Services
+{
+    public class UserInfoService : UserInfoProtoService.UserInfoProtoServiceBase
+    {
+
+        private readonly IUserInformationRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<UserInfoService> _logger;
+
+        public UserInfoService(IUserInformationRepository repository, IMapper mapper, ILogger<UserInfoService> logger)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async override Task<GetUserInfoResponse> GetUserInfo(GetUserInfoRequest request, ServerCallContext context)
+        {
+            var userInfo = await _repository.GetUserInfo(request.Id)
+                ?? throw new RpcException(new Status(StatusCode.NotFound, $"UserInfo with Id = {request.Id} is not found"));
+
+            _logger.LogInformation("UserInfo with ID: {mangaId} is retrieved", request.Id);
+
+            var response = new GetUserInfoResponse();
+            response.ReadingJournal.LastReadMangaID = userInfo.ReadingJournal.LastReadMangaID;
+            response.ReadingJournal.AllReadMangaIDs.AddRange(userInfo.ReadingJournal.AllReadMangaIDs);
+            response.ReadingJournal.WishList.AddRange(userInfo.ReadingJournal.WishList);
+            return response;
+        }
+    }
+}
