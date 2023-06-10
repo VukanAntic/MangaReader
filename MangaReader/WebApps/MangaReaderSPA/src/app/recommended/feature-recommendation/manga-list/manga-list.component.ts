8a9cca04c67eb68e9836d052f7fa56f646a7e72e
mi18145 <mi18145@alas.matf.bg.ac.rs>
2023-06-10 21:51:42 +0200

    Add: All lists that were meant to be on the Recommendation page
    Changed a lot of css, made Recommendation page look decent, added scrolling functionallity with arrows, Recommendation page now has all necessarry components

--- MangaReader/WebApps/MangaReaderSPA/src/app/recommended/feature-recommendation/manga-list/manga-list.component.ts
+++ MangaReader/WebApps/MangaReaderSPA/src/app/recommended/feature-recommendation/manga-list/manga-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core'
+import { Component, HostListener, Input, OnInit } from '@angular/core'
 import { Manga } from '../../domain/models/manga.model'
 
 @Component({
@@ -9,7 +9,58 @@ import { Manga } from '../../domain/models/manga.model'
 export class MangaListComponent implements OnInit {
   @Input()
   public mangaList?: Manga[]
-  constructor() {}
 
-  ngOnInit(): void {}
+  public currentPage: number = 0
+  public displayMangaList?: Manga[]
+  public innerWidth: number
+  public paginationLimit: number
+
+  constructor() {
+    this.innerWidth = window.innerWidth
+    this.paginationLimit = Math.floor((this.innerWidth - 870) / 150.0)
+  }
+
+  ngOnInit(): void {
+    this.currentPage = 0
+    this.innerWidth = window.innerWidth
+    this.displayMangaList = this.mangaList?.slice(
+      this.currentPage * this.paginationLimit,
+      (this.currentPage + 1) * this.paginationLimit,
+    )
+    console.log('??')
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.innerWidth = window.innerWidth
+    console.log(this.innerWidth)
+    this.paginationLimit = Math.floor((this.innerWidth - 870) / 150.0)
+    console.log(this.mangaList)
+    this.ngOnChanges()
+  }
+
+  ngOnChanges(): void {
+    this.displayMangaList = this.mangaList?.slice(
+      this.currentPage * this.paginationLimit,
+      Math.min(
+        (this.currentPage + 1) * this.paginationLimit,
+        this.mangaList.length,
+      ),
+    )
+    //console.log(this.mangaList)
+  }
+
+  nextPage() {
+    this.currentPage += 1
+    if (this.currentPage * this.paginationLimit >= this.mangaList!.length)
+      this.currentPage = 0
+    this.ngOnChanges()
+  }
+
+  previousPage() {
+    this.currentPage -= 1
+    if (this.currentPage < 0)
+      this.currentPage = this.mangaList!.length - this.paginationLimit - 1
+    this.ngOnChanges()
+  }
 }
