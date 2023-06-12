2b81d925579e060b65524d0e77e2732559c4820e
mi18145 <mi18145@alas.matf.bg.ac.rs>
2023-06-12 23:24:05 +0200

    Change: Removed headers from Recommended call
    Using the new intercepter we don't need headers in our Recommended call anymore

--- MangaReader/WebApps/MangaReaderSPA/src/app/recommended/domain/infrastructure/recommendation.service.ts
+++ MangaReader/WebApps/MangaReaderSPA/src/app/recommended/domain/infrastructure/recommendation.service.ts
@@ -1,22 +1,15 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { Injectable } from '@angular/core'
-import { Observable } from 'rxjs'
-import { RecommendationPage } from '../models/recommendation-page.model'
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { RecommendationPage } from "../models/recommendation-page.model";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class RecommendationService {
   constructor(private httpClient: HttpClient) {}
 
-  public getRecommenationPage(
-    accessToken?: string,
-  ): Observable<RecommendationPage> {
-    return this.httpClient.get<RecommendationPage>(
-      'http://localhost:8004/api/Recommendation/GetRecommendationPage',
-      {
-        headers: { Authorization: `Bearer ${accessToken}` },
-      },
-    )
+  public getRecommenationPage(accessToken?: string): Observable<RecommendationPage> {
+    return this.httpClient.get<RecommendationPage>("http://localhost:8004/api/Recommendation/GetRecommendationPage");
   }
 }
